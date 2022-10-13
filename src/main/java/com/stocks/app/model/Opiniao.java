8f41f71a13379d06989002dfe2c3a837597d844e
diogoasp97 <diogoasp97@gmail.com>
2022-10-13 22:03:14 +0100

    Date field on backend and CORS

--- src/main/java/com/stocks/app/model/Opiniao.java
+++ src/main/java/com/stocks/app/model/Opiniao.java
@@ -1,5 +1,7 @@
 package com.stocks.app.model;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,11 +16,14 @@ public class Opiniao {
 	@Column(nullable = false, updatable = false)
 	private long opiniaoId;
 	private String textoOpiniao;
+	private Date date;
 	
 	public Opiniao() {
+		
 	}
 	
-	public Opiniao(String textoOpiniao) {
+	public Opiniao(String textoOpiniao, Date date) {
+		this.date = date;
 		this.textoOpiniao = textoOpiniao;
 	}
 
@@ -29,9 +34,17 @@ public class Opiniao {
 	public void setTextoOpiniao(String textoOpiniao) {
 		this.textoOpiniao = textoOpiniao;
 	}
+	
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
 
 	@Override
 	public String toString() {
-		return "Opiniao [opiniaoId=" + opiniaoId + ", textoOpiniao=" + textoOpiniao + "]";
+		return "Opiniao [opiniaoId=" + opiniaoId + ", textoOpiniao=" + textoOpiniao + ", date=" + date + "]";
 	}
 }
